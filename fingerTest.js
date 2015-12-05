5e1195c8aeff1c88231dc54f3491aae56e50a37a
Abhishek Mishra <abhishek@Abhisheks-MBP.home>
2015-12-04 20:05:30 -0500

    fingerTest works

--- fingerTest.js
+++ fingerTest.js
@@ -4,17 +4,43 @@ Cylon
 	.robot()
 	.connection("leapmotion", { adaptor: "leapmotion" })
 	.device("leapmotion", { driver: "leapmotion" })
-	.on("ready", function(bot) {
-		var pointers = 0;
-		bot.leapmotion.on("hand", function(hand) {
-			for (item in hand.pointables) {
-				// console.log(hand.pointables.length)
-				if (item["extended"] == true) {
-					pointers++;
-				}
-			}
-			console.log(pointers);
+	.on("ready", function(bot) 
+	{
+		bot.leapmotion.on("hand", function(hand) 
+		{
+			var fingers = [hand.thumb.extended,
+			hand.indexFinger.extended,
+			hand.middleFinger.extended,
+			hand.ringFinger.extended,
+			hand.pinky.extended];
+			console.log(RPS(fingers));
 		});
 	});
 
 Cylon.start()
+
+function RPS(fingers){
+	if (fingers[1] && fingers[2] && !fingers[0] && !fingers[3] && !fingers[4])
+		return "scissors";
+	if (fingers[0] && fingers[1] && fingers[2] && fingers[3] && fingers[4])
+		return "paper";
+	else
+		return "rock";
+}
+
+function compare(p1, p2){
+	if (p1 === p2):
+		return "tie"
+	if (p1 === "scissors" && p2 === "rock")
+		return p2;
+	if (p1 === "rock" && p2 === "scissors")
+		return p1;
+	if (p1 === "paper" && p2 === "scissors")
+		return p2;
+	if (p1 === "scissors" && p2 === "paper")
+		return p1;
+	if (p1 === "rock" && p2 === "paper")
+		return p2;
+	if (p1 === "paper" && p2 === "rock")
+		return p
+}
