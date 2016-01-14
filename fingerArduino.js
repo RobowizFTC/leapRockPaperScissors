567ee2a8e6818a3642eef24b963888b837709b10
AdarshK1 <addy2999@gmail.com>
2016-01-14 17:31:37 -0500

    sdf

--- fingerArduino.js
+++ fingerArduino.js
@@ -1,30 +1,44 @@
+// var Cylon = require('cylon');
+
+// Cylon.robot({
+// 	connections:{
+// 		arduino: {adaptor: 'firmata', port: '/dev/tty.usbmodem1411'}
+// 	},
+
+// 	devices: {
+//     	servo: {driver: 'continuous-servo', pin:3}
+//   },
+
+//   work: function(my) {
+//     	var clockwise = true;
+
+//     	my.servo.clockwise();
+
+//     	every((1).second(), function() {
+//       	if (clockwise) {
+//         	my.servo.counterClockwise();
+//         	clockwise = false;
+//       	} else {
+//         	my.servo.clockwise();
+//         	clockwise = true;
+//       	}
+//     });
+//   	}
+//   }
+
+// }).start()
 var Cylon = require('cylon');
 
 Cylon.robot({
-	connections:{
-		leap: {adaptor: 'leapmotion'},
-		arduino: {adaptor: 'firmata', port: '/dev/tty.usbmodem1421'}
-	},
+  connections: {
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+  },
 
-	devices: {
-    	servo: {driver: 'continuous-servo', pin:3}
+  devices: {
+    led: { driver: 'led', pin: 13 }
   },
 
   work: function(my) {
-    	var clockwise = true;
-
-    	my.servo.clockwise();
-
-    	every((1).second(), function() {
-      	if (clockwise) {
-        	my.servo.counterClockwise();
-        	clockwise = false;
-      	} else {
-        	my.servo.clockwise();
-        	clockwise = true;
-      	}
-    });
-  	}
+    every((1).second(), my.led.toggle);
   }
-
-}).start()
+}).start();
